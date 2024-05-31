997b018bfd216330a4e5fd76355f9046914e2490
shout64 <135728867+shout64@users.noreply.github.com>
2024-05-31 16:39:29 -0500

    Clean up and comment code

--- addons/uid_refresh/uid_refresh.gd
+++ addons/uid_refresh/uid_refresh.gd
@@ -10,50 +10,53 @@ func _enter_tree():
   if ProjectSettings.get_setting("debug/file_logging/enable_file_logging") == false:
     ProjectSettings.set_setting("debug/file_logging/enable_file_logging", true)
 
-#### Default log location user://logs/godot.log
-# Windows: %APPDATA%\Godot\app_userdata\[project_name]
-# macOS: ~/Library/Application Support/Godot/app_userdata/[project_name]
-# Linux: ~/.local/share/godot/app_userdata/[project_name]
-  log_location = ProjectSettings.get_setting("debug/file_logging/log_path")
-
 func _exit_tree():
   remove_tool_menu_item(tool_name)
 
 func fix_broken_uid():
-  check_log_location()
+  get_log_location()
   fix_broken_dependancies()
 
 
-func check_log_location():
+func get_log_location():
   # If log location changes, update it.
+  
+  #### Default log location user://logs/godot.log
+# Windows: %APPDATA%\Godot\app_userdata\[project_name]
+# macOS: ~/Library/Application Support/Godot/app_userdata/[project_name]
+# Linux: ~/.local/share/godot/app_userdata/[project_name]
+
   if ProjectSettings.get_setting("debug/file_logging/log_path") != log_location:
     log_location = ProjectSettings.get_setting("debug/file_logging/log_path")
 
 func fix_broken_dependancies():
-  var log = FileAccess.open("user://logs/godot.log", FileAccess.READ)
+  # Open the error log
+  var log = FileAccess.open(log_location, FileAccess.READ)
   while log.eof_reached() == false:
     var content = log.get_line()
     if content.contains("invalid UID: ") == true:
       # Find the affected TSCN file
-      var tscn_loc = (content.find(".tscn") - 9)
+      var tscn_loc      = (content.find(".tscn") - 9)
       var affected_tscn = content.substr(14, tscn_loc)
 
       # Find invalid UID
-      var uid_start = (content.find("invalid UID: ") + 13)
-      var uid_end = (content.find(" - using", uid_start) - uid_start)
+      var uid_start   = (content.find("invalid UID: ") + 13)
+      var uid_end     = (content.find(" - using", uid_start) - uid_start)
       var invalid_uid = content.substr(uid_start, uid_end)
 
       # Find path of asset
       var asset_start = content.find("res://", uid_end)
-      var asset_path = content.substr(asset_start, -1)
+      var asset_path  = content.substr(asset_start, -1)
       
       # Get correct UID for assets
-      var asset_UID_text = ResourceLoader.get_resource_uid(asset_path)
+      var asset_UID_text    = ResourceLoader.get_resource_uid(asset_path)
       var correct_asset_UID = ResourceUID.id_to_text(asset_UID_text)
 
-      # TODO: Replace UID not working yet
-      var tscn = FileAccess.open(affected_tscn, FileAccess.READ_WRITE)
+      # Open affected TSCN
+      var tscn             = FileAccess.open(affected_tscn, FileAccess.READ_WRITE)
       var tscn_content_old = tscn.get_as_text()
+      
+      # Fix the mismatched UID, and don't try to overwrite if it's already been fixed
       if tscn_content_old.contains(invalid_uid) == true:
         var tscn_content_new = tscn_content_old.replacen(invalid_uid, correct_asset_UID)
         tscn.store_string(tscn_content_new)
